894fda4c55e34015ad06e30c2b7eaf7943254dad
Haziq Naeem <haziqnaeem36@gmail.com>
2022-04-23 19:42:32 -0400

    post and comment model

---
+++ models/post.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+  {
+    comment_text: {
+      type: String,
+      required: true
+    },
+    codeblock: {
+      type: String,
+      required: false
+    },
+    is_solution: {
+      type: Boolean,
+      default: false
+    },
+    commenter: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }
+  },
+  { timestamps: true }
+)
+
+const postSchema = new Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  codeblock: {
+    type: String,
+    required: false
+  },
+  is_resolved: {
+    type: Boolean,
+    default: false
+  },
+  added_by: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'Profile'
+  },
+  comments: [commentSchema]
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+export {
+  Post
+}
--- models/profile.js
+++ models/profile.js
@@ -7,6 +7,15 @@ const profileSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String, 
+      required: true
+    },
+    posts: [
+      {
+      type: mongoose.Schema.Types.ObjectId, ref: 'Post'
+    }
+  ]
   },
   { timestamps: true }
 )
