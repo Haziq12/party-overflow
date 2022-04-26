a5ef29f7c0b1c86bf992fa7a47db0e1f1e6d7cb5
Haziq Naeem <haziqnaeem36@gmail.com>
2022-04-26 18:21:49 -0400

    nav bar links

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Nav from '../components/Nav/Nav'
 //Services
 
 //Pages + Components
@@ -8,11 +9,16 @@ const App = () => {
 
   return (
     <div className="App">
+      
+      <Nav />
+
       <Routes>
+
         <Route path='/' element={<h1>Landing</h1>} />
         <Route path='/signin' element={<h1>Signin</h1>} />
         <Route path='/signup' element={<h1>Signup</h1>} />
         <Route path='/posts' element={<h1>Posts</h1>} />
+
       </Routes>
     </div>
   )
