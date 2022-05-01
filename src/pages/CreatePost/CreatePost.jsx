2037f4f54c7d054549da19b53d9b8378c104f599
Haziq Naeem <haziqnaeem36@gmail.com>
2022-05-01 13:46:40 -0400

    handleCreatePost function

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { createPost } from '../../services/postService'
+import { useNavigate } from 'react-router'
+
 import '../../styles/Create.css'
 
 // Components
@@ -6,6 +9,20 @@ import Header from '../../components/misc/Header'
 import PostForm from './PostForm'
 
 const CreatePost = (props) => {
+
+  const navigate = useNavigate()
+  
+  const handleCreatePost = async (e) => {
+    e.preventDefault()
+    try {
+      const newPost = await createPost(formData) 
+      console.log(newPost) 
+      navigate('/posts') 
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
     <div className="layout">
       <Header />
