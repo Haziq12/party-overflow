ef4872f83c39227ec1e19e37c0c76acca7d67b2a
Haziq Naeem <haziqnaeem36@gmail.com>
2022-05-01 13:36:44 -0400

    import and mount Header, PostForm compnent to CreatePost

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 
 // Components
+import Header from '../../components/misc/Header'
+import PostForm from './PostForm'
 
 const CreatePost = (props) => {
   return (
     <div className="layout">
- 
+      <Header />
+      <PostForm />
     </div>
   )
 }
