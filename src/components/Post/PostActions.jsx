25a9d76dde069ff82c648150a8b48c0b7dca2d6f
Haziq Naeem <haziqnaeem36@gmail.com>
2022-05-01 14:12:29 -0400

    PostActions biolerplate

---
+++ src/components/Post/PostActions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../styles/Card.css'
+
+const PostActions = (props) => {
+
+  return (
+    <div className="interactions">
+
+    </div>
+  )
+}
+
+export default PostActions
